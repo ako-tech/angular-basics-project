c9688757c5aa864951a782920f812607d630c932
akotech <akotech20@gmail.com>
2021-10-24 16:58:51 +0200

    remove contactModule exports

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -5,6 +5,5 @@ import { ContactComponent } from './contact.component';
 @NgModule({
   declarations: [ContactComponent],
   imports: [CommonModule],
-  exports: [ContactComponent],
 })
 export class ContactModule {}
