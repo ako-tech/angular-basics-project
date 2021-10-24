36788a829036b7dd67bbfcc07c999a46c38c4e1b
akotech <akotech20@gmail.com>
2021-10-24 17:00:29 +0200

    add ContactModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { ShoppingCartModule } from './shopping-cart/shopping-cart.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavDrawerComponent } from './nav-drawer/nav-drawer.component';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, NavDrawerComponent],
-  imports: [BrowserModule, ShoppingCartModule, CatalogModule],
+  imports: [BrowserModule, ShoppingCartModule, CatalogModule, ContactModule],
   providers: [],
   bootstrap: [AppComponent],
 })
